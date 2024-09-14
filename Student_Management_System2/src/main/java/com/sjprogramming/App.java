30263d433fd93cc325b8e77c32185153d9d647fd
nick6254 <95649674+nick6254@users.noreply.github.com>
2024-09-14 14:11:09 +0530

    Add files via upload

---
+++ Student_Management_System2/src/main/java/com/sjprogramming/App.java
@@ -0,0 +1,13 @@
+package com.sjprogramming;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
